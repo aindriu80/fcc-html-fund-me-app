a24d33c856b50bed47b37d3bcf1e23ba713a8f09
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-14 12:14:41 +0100

    Adding try catch

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,14 @@
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
-    await window.ethereum.request({ method: 'eth_requestAccounts' })
-    console.log('Connected....')
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+      console.log('Connected....')
+    } catch (error) {
+      console.log(error)
+    }
     document.getElementById('connectButton').innerHTML = 'Connected'
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
+    console.log(accounts)
   } else {
     document.getElementById('connectButton').innerHTML =
       'Please install MetaMask'
