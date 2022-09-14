1b40365bf90b5772083edd8eea27c74873c0fed4
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-14 13:32:24 +0100

    Adding Ethers

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,35 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
 async function connect() {
-  if (typeof window.ethereum !== 'undefined') {
-    try {
-      await window.ethereum.request({ method: 'eth_requestAccounts' })
-      console.log('Connected....')
-    } catch (error) {
-      console.log(error)
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await window.ethereum.request({ method: "eth_requestAccounts" })
+            console.log("Connected....")
+            console.log(ethers)
+        } catch (error) {
+            console.log(error)
+        }
+        document.getElementById("connectButton").innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
+    } else {
+        document.getElementById("connectButton").innerHTML =
+            "Please install MetaMask"
     }
-    document.getElementById('connectButton').innerHTML = 'Connected'
-    const accounts = await ethereum.request({ method: 'eth_accounts' })
-    console.log(accounts)
-  } else {
-    document.getElementById('connectButton').innerHTML =
-      'Please install MetaMask'
-  }
 }
+
+// Fund function
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        // provider / connection to the blockchain
+        // signer / wallet / someone with some gas
+        // contract that we are interacting with
+    }
+}
+// Withdraw function
