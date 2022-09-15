1e2fc8868a18c559612cc1e2558503223415ca7b
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-15 14:24:43 +0100

    Sending a Transaction from a Website

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,5 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "../../constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
@@ -14,22 +15,32 @@ async function connect() {
         } catch (error) {
             console.log(error)
         }
-        document.getElementById("connectButton").innerHTML = "Connected"
+        connectButton.innerHTML = "Connected"
         const accounts = await ethereum.request({ method: "eth_accounts" })
         console.log(accounts)
     } else {
-        document.getElementById("connectButton").innerHTML =
-            "Please install MetaMask"
+        connectButton.innerHTML = "Please install MetaMask"
     }
 }
 
 // Fund function
-async function fund(ethAmount) {
+async function fund() {
+    const ethAmount = ".01"
     console.log(`Funding with ${ethAmount}...`)
     if (typeof window.ethereum !== "undefined") {
         // provider / connection to the blockchain
         // signer / wallet / someone with some gas
         // contract that we are interacting with
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 // Withdraw function
