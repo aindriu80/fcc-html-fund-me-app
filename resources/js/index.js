797d43fdbf009f84964337998b3216593eb95940
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-16 17:07:42 +0100

    Listening for Events and Completed Transactions

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
-import { abi, contractAddress } from "../../constants.js"
+import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
@@ -38,9 +38,27 @@ async function fund() {
             const transactionResponse = await contract.fund({
                 value: ethers.utils.parseEther(ethAmount),
             })
+            // listen for the tx to be mined
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done!")
         } catch (error) {
             console.log(error)
         }
     }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}....`)
+    // return new Promise
+    // create a listener for the blockchain
+    // listen for this transaction to finish
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            )
+            resolve()
+        })
+    })
 }
 // Withdraw function
